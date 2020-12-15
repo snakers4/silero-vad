ab55c0d7382646d853b32cafe358bfc85f204a26
snakers41 <aveysov@gmail.com>
2020-12-15 12:42:56 +0000

    Polish hubconf file

--- hubconf.py
+++ hubconf.py
@@ -1,28 +1,25 @@
 dependencies = ['torch', 'torchaudio', 'numpy']
 import torch
 from omegaconf import OmegaConf
-from utils import (init_jit_model, 
+from utils import (init_jit_model,
                    get_speech_ts,
-                   save_audio, 
-                   read_audio, 
-                   state_generator, 
+                   save_audio,
+                   read_audio,
+                   state_generator,
                    single_audio_stream,
                    collect_speeches)
 
 
-def silero_stt(**kwargs):
-    """Silero Voice Activity and Number Detector Models
-    Returns a model and a set of utils
+def silero_vad(**kwargs):
+    """Silero Voice Activity Detector, Number Detector and Language Classifier
+    Returns a model with a set of utils
     Please see https://github.com/snakers4/silero-vad for usage examples
     """
-    torch.hub.download_url_to_file('https://raw.githubusercontent.com/snakers4/silero-vad/master/files/model.jit',
-                                   'files/model.jit',
-                                   progress=False)
-    model = init_jit_model(model_url='files/model.jit')
+    model = init_jit_model(model_path='files/model.jit')
     utils = (get_speech_ts,
-             save_audio, 
-             read_audio, 
-             state_generator, 
+             save_audio,
+             read_audio,
+             state_generator,
              single_audio_stream,
              collect_speeches)
 
