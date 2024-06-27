06b9e17c1e12284fc59643899de356fc1398e031
Alexander Veysov <aveysov@gmail.com>
2024-06-27 22:56:37 +0300

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -4,9 +4,6 @@ import json
 import os
 from utils_vad import (init_jit_model,
                        get_speech_timestamps,
-                       get_number_ts,
-                       get_language,
-                       get_language_and_group,
                        save_audio,
                        read_audio,
                        VADIterator,
@@ -51,64 +48,5 @@ def silero_vad(onnx=False, force_onnx_cpu=False):
              collect_chunks)
 
     return model, utils
-
-
-def silero_number_detector(onnx=False, force_onnx_cpu=False):
-    """Silero Number Detector
-    Returns a model with a set of utils
-    Please see https://github.com/snakers4/silero-vad for usage examples
-    """
-    raise NotImplementedError('This model has been deprecated and is not supported anymore.')
-    if onnx:
-        url = 'https://models.silero.ai/vad_models/number_detector.onnx'
-    else:
-        url = 'https://models.silero.ai/vad_models/number_detector.jit'
-    model = Validator(url, force_onnx_cpu)
-    utils = (get_number_ts,
-             save_audio,
-             read_audio,
-             collect_chunks,
-             drop_chunks)
-
-    return model, utils
-
-
-def silero_lang_detector(onnx=False, force_onnx_cpu=False):
-    """Silero Language Classifier
-    Returns a model with a set of utils
-    Please see https://github.com/snakers4/silero-vad for usage examples
-    """
-    raise NotImplementedError('This model has been deprecated and is not supported anymore.')
-    if onnx:
-        url = 'https://models.silero.ai/vad_models/number_detector.onnx'
-    else:
-        url = 'https://models.silero.ai/vad_models/number_detector.jit'
-    model = Validator(url, force_onnx_cpu)
-    utils = (get_language,
-             read_audio)
-
-    return model, utils
-
-
-def silero_lang_detector_95(onnx=False, force_onnx_cpu=False):
-    """Silero Language Classifier (95 languages)
-    Returns a model with a set of utils
-    Please see https://github.com/snakers4/silero-vad for usage examples
-    """
-    raise NotImplementedError('This model has been deprecated and is not supported anymore.')
-    if onnx:
-        url = 'https://models.silero.ai/vad_models/lang_classifier_95.onnx'
-    else:
-        url = 'https://models.silero.ai/vad_models/lang_classifier_95.jit'
-    model = Validator(url, force_onnx_cpu)
-
-    model_dir = os.path.join(os.path.dirname(__file__), 'files')
-    with open(os.path.join(model_dir, 'lang_dict_95.json'), 'r') as f:
-        lang_dict = json.load(f)
-
-    with open(os.path.join(model_dir, 'lang_group_dict_95.json'), 'r') as f:
-        lang_group_dict = json.load(f)
-
-    utils = (get_language_and_group, read_audio)
 
     return model, lang_dict, lang_group_dict, utils
