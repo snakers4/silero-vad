6217b08bbbd7650563372f71e1bdd716ce191a7e
adamnsandle <dvoronin322@gmail.com>
2024-11-12 08:25:06 +0000

    add other opsets

--- src/silero_vad/model.py
+++ src/silero_vad/model.py
@@ -2,10 +2,21 @@ from .utils_vad import init_jit_model, OnnxWrapper
 import torch
 torch.set_num_threads(1)
 
-def load_silero_vad(onnx=False):
-    model_name = 'silero_vad.onnx' if onnx else 'silero_vad.jit'
+
+def load_silero_vad(onnx=False, opset_version=16):
+    available_ops = [13, 14, 15, 16]
+    if onnx and opset_version not in available_ops:
+        raise Exception(f'Available ONNX opset_version: {available_ops}')
+
+    if onnx:
+        if opset_version == 16:
+            model_name = 'silero_vad.onnx'
+        else:
+            model_name = f'silero_vad_op{opset_version}.onnx'
+    else:
+        model_name = 'silero_vad.jit'
     package_path = "silero_vad.data"
-    
+
     try:
         import importlib_resources as impresources
         model_file_path = str(impresources.files(package_path).joinpath(model_name))
@@ -21,5 +32,5 @@ def load_silero_vad(onnx=False):
         model = OnnxWrapper(model_file_path, force_onnx_cpu=True)
     else:
         model = init_jit_model(model_file_path)
-    
+
     return model
